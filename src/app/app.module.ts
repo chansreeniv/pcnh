87d6217c20f49e0e711aed6c20dec6ad20392c27
chansreeniv <chansreeniv@yahoo.com>
2022-03-31 23:28:58 +0530

    bootstrap added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { SearchComponent } from './search/search.component';
+import { LoginComponent } from './login/login.component';
+import { ResultsComponent } from './results/results.component'; 
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SearchComponent
+    SearchComponent,
+    LoginComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     BrowserAnimationsModule,
-    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigService } from '../services/config.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  navLogin: string = "nav-link";
+  navSearch: string = "nav-link";
 
-  constructor() { }
+  constructor(private configService: ConfigService) { }
 
   ngOnInit(): void {
+    this.navLogin= "nav-link";
+    this.navSearch = "nav-link";
+    this.configService.getConfig().subscribe((response)=>{console.log(response)});
   }
+
+  onLogin(){
+    this.navLogin = "nav-link active";
+  }
+
+  onSearch(){
+    this.navSearch = "nav-link active"
+  }
+
+  // onDeactivate(){
+  //   this.navSearch = "nav-link disabled"
+  // }
 
 }
