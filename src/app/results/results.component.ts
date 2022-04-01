c46662256c86fa5c12bedb3d3b0dba457f265301
chansreeniv <chansreeniv@yahoo.com>
2022-04-01 10:06:00 +0530

    basic change to UI

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./results.component.css']
 })
 export class ResultsComponent implements OnInit {
+  responseData = [{
+    "uhid":"20221001",
+    "name": "Alpha",
+    "age": "13",
+    "sex":"female",
+    "phone": "123456789",
+    "lastVisit":"12-2-2022 8:00a.m",
+  },{
+    "uhid":"20221002",
+    "name": "mike",
+    "age": "15",
+    "sex":"male",
+    "phone": "123456789",
+    "lastVisit":"12-1-2022 11:00a.m",
+  }]
+
+  notFound = true;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
